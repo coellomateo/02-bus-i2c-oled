532345858e77ede6a1270cf15a9acab6d75fc618
coellomateo <menacoelllomateo@gmail.com>
2026-09-14 22:35:23 -0500

    feat: Entrega final Semana 02 - Mateo Coello

--- bloque_1/src/bloque_1.ino
+++ bloque_1/src/bloque_1.ino
@@ -19,32 +19,27 @@ void setup() {
     Serial.println("  [BLOQUE 1] ESCÁNER DE DIRECCIONES DE HARDWARE I2C     ");
     Serial.println("========================================================");
 
-    // TODO 1.1: Inicializar el bus I2C en los pines SDA y SCL del ESP32.
-    // Pregunta Guía: ¿Qué función de la librería Wire recibe (SDA_PIN, SCL_PIN)?
      Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+    
 
-    // TODO 1.2: Configurar la velocidad del reloj a 400kHz (Modo Rápido).
-    // Pregunta Guía: ¿Qué función configura la frecuencia de reloj del bus I2C?
+    
      Wire.setClock(I2C_CLOCK_SPEED);
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+    
 
     Serial.println("[I2C] Bus configurado en SDA:GPIO21, SCL:GPIO22 a 400kHz.\n");
     Serial.println("--- INICIANDO BARRIDO DE DIRECCIONES (0x01 .. 0x7E) ---");
 
     int devicesFound = 0;
 
-    // TODO 1.3: Recorrer las direcciones válidas de 7 bits (del 1 al 126).
+    
     for (byte address = 1; address < 127; address++) {
 
-        // TODO 1.4: Tocar la puerta del periférico:
-        // 1. Iniciar transmisión con Wire.beginTransmission(address);
-        // 2. Finalizar transmisión capturando el byte de error: byte error = Wire.endTransmission();
+        
         Wire.beginTransmission(address);     // Toca la puerta del sensor
         byte error = Wire.endTransmission(); // 0 = ¡Presente! (Pulso ACK)
 
-        // TODO 1.5: Evaluar la respuesta del periférico:
-        // Pregunta Guía: ¿Qué valor devuelve Wire.endTransmission() cuando el periférico responde con ACK (Presente)?
+        
+       
          if (error == 0) {
             Serial.printf("[I2C] Dispositivo detectado en: 0x%02X ", address);
              if (address == OLED_I2C_ADDR) {
